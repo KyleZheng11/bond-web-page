e7b7b07e45c6776cbc9509bb105e9e5ad6b4ed70
KyleZheng11 <k1105yle@gmail.com>
2026-05-19 22:25:29 -0400

    Adding some content, working on creating and formatting Claude.md file

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,10 +5,39 @@ export const Route = createFileRoute('/')({ component: Home })
 function Home() {
   return (
     <div className="p-8">
-      <h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
-      <p className="mt-4 text-lg">
-        Edit <code>src/routes/index.tsx</code> to get started.
-      </p>
+      <h1 className="text-4xl font-bold text-center">Welcome to Bond</h1>
+
+      <div className="flex-row">
+        <div className="justify-self-center">
+          <p className="mt-4 text-lg">
+            Stop Planning
+          </p>
+          <p>
+            Start going out.
+          </p>
+          <p>
+            Bond makes being with your friends easy and seamless, no matter the situation.
+          </p>
+          <button className="border-1 hover:bg-sky-400">
+            Get early access
+          </button>
+        </div>
+      </div>
+
+      <p>Everything your friend group needs</p>
+      <div>
+        <p>Discover</p>
+        <p>Plan Together</p>
+        <p>Just show up</p>
+      </div>
+
+      <div>Our mission</div>
+
+
+      <div>
+        Get early access...waitlist
+      </div>
+      
     </div>
   )
 }
